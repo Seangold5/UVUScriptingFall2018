aa38dbf724efb46436b990310b1018e9119f9ed5
Daniel Bredthauer <dcarlbred@gmail.com>
2018-09-12 17:16:41 -0600

    Created CharacterMove script

--- My Game/Assets/Script/CharacterMove.cs
+++ My Game/Assets/Script/CharacterMove.cs
@@ -4,16 +4,39 @@ using UnityEngine;
 
 public class CharacterMove : MonoBehaviour {
 
-	public int MoveSpeed
-	public int JumpHeight
+	// Player Movement Variables
+	public int MoveSpeed;
+	public float JumpHeight;
+
+	// Player Grounded Variables
+	public Transform GroundCheck;
+	public float GroundCheckRadius;
+	public LayerMask WhatIsGround;
+	private bool Grounded;
 
 	// Use this for initialization
 	void Start () {
 		
 	}
 	
+
+	void FixedUpdate () {
+		Grounded = Physics2D.OverlapCircle(GroundCheck.Position, GroundCheckRadius, WhatIsGround);
+	}
+
 	// Update is called once per frame
 	void Update () {
 		
+		// This code makes the character jump
+		if(Input.GetKeyDown (KeyCode.Space)&& Grounded){
+			Jump();
+		}
+	
+		// This code makes the character move from side to side using the A&D keys
+
+	}
+
+	public void Jump(){
+		GetComponent<Rigidbody2D>().velocity = new Vector2(GetComponent<Rigidbody2D>().velocity.x, JumpHeight);
 	}
 }
