ad668ab647b0f64cc0eaa24d18515070990ea1ee
Daniel Bredthauer <dcarlbred@gmail.com>
2018-09-18 20:51:58 -0600

    Fixing problems with program

---
+++ My Game Fall 2018/Assets/Script/CharacterMove.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterMove : MonoBehaviour {
+
+	// Player Movement Variables
+	public int MoveSpeed;
+	public float JumpHeight;
+
+	// Player Grounded Variables
+	public Transform GroundCheck;
+	public float GroundCheckRadius;
+	public LayerMask WhatIsGround;
+	private bool Grounded;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+
+	void FixedUpdate () {
+		Grounded = Physics2D.OverlapCircle(GroundCheck.Position, GroundCheckRadius, WhatIsGround);
+	}
+
+	// Update is called once per frame
+	void Update () {
+		
+		// This code makes the character jump
+		if(Input.GetKeyDown (KeyCode.Space)&& Grounded){
+			Jump();
+		}
+	
+		// This code makes the character move from side to side using the A&D keys
+		if(Input.GetKey (KeyCode.D)){
+			GetComponent<Rigidbody2D>().velocity = new Vector2(MoveSpeed, GetComponent<Rigidbody2D>().velocity.y);
+
+		}
+		if(Input.GetKey (KeyCode.A)){
+			GetComponent<Rigidbody2D>().velocity = new Vector2(-MoveSpeed, GetComponent<Rigidbody2D>().velocity.y);
+
+		}
+		
+	}
+
+	public void Jump(){
+		GetComponent<Rigidbody2D>().velocity = new Vector2(GetComponent<Rigidbody2D>().velocity.x, JumpHeight);
+	}
+}
