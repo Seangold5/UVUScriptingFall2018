99c180a4b5b2af156e61beb822f8eb750bcdde9d
Daniel Bredthauer <dcarlbred@gmail.com>
2018-10-03 17:16:53 -0600

    Added Level Manager Code

---
+++ My Game Fall 2018/Assets/Script/LevelManager.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelManager : MonoBehaviour {
+
+	public GameObject CurrentCheckPoint;
+	private Rigidbody2D Player;
+
+	// Particles
+	public GameObject DeathParticle;
+	public GameObject RespawnParticle;
+
+	// Respawn Delay
+	public float RespawnDelay;
+
+	// Point Penalty on Death
+	public int PointPenaltyOnDeath;
+
+	// Store Gravity Value
+	private float GravityStore;
+
+
+	// Use this for initialization
+	void Start () {
+		Player = FindObjectOfType<Rigidbody2D> ();
+	}
+
+	public void RespawnPlayerCo(){
+		StartCoroutine ("RespawnPlayerCo");
+	}
+
+	public IEnumerator RespawnPlayerCo(){
+		// Generate Death Particle
+		Instantiate (DeathParticle, Player.transform.position, Player.transform. rotation);
+		// Hide Player
+		// Player.enabled = false;
+		Player.GetComponent<Renderer> ().enabled = false;
+		// Gravity Reset
+		GravityStore = Player.GetComponent<Rigidbody2D>().GravityScale;
+		Player.GetComponent<Rigidbody2D>().GravityScale = 0f;
+		Player.GetComponent<Rigidbody2D>().velocity = Vector2.zero;
+		// Point Penalty
+		ScoreManager.AddPoints(-PointsPenaltyOnDeath);
+		// Debug Message
+		Debug.Log ("Player Respawn");
+
+	}
+}
