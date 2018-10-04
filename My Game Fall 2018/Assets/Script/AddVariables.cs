ce5e3ac34acdf76df652116bf6cefcba63e4d616
Daniel Bredthauer <dcarlbred@gmail.com>
2018-10-04 09:42:22 -0600

    Worked on calculator script

---
+++ My Game Fall 2018/Assets/Script/AddVariables.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AddVariables : MonoBehaviour {
+
+	// Variables
+	public int VariableOne;
+	public int VariableTwo;
+	private int TotalVariable;
+
+	// Use this for initialization
+	void Start () {
+		Add(TotalVariable = VariableOne+VariableTwo);
+		
+		Print(TotalVariable)
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
