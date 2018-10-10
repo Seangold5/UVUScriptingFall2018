03febadf5d9aae757f3185b55fd85b5e18ab2e9d
Daniel Bredthauer <dcarlbred@gmail.com>
2018-10-10 16:35:19 -0600

    Practiced writing scripts

---
+++ My Game Fall 2018/Assets/Script/Practice Scrpts/Snake.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Snake : Reptile {
+
+	public int length;
+	public bool venomous;
+	public bool aquatic;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
