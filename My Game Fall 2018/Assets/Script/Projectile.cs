a5a13cdc9facdf5867444d947c2023a7ad234f77
Daniel Bredthauer <dcarlbred@gmail.com>
2018-10-17 17:13:46 -0600

    Added more to the level

--- My Game Fall 2018/Assets/Script/Projectile.cs
+++ My Game Fall 2018/Assets/Script/Projectile.cs
@@ -1,45 +1,45 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿// using System.Collections;
+// using System.Collections.Generic;
+// using UnityEngine;
 
-public class Projectile : MonoBehaviour {
+// public class Projectile : MonoBehaviour {
 
-	public float Speed;
-	public Rigidbody2D PC;
+// 	public float Speed;
+// 	public Rigidbody2D PC;
 
-	public GameObject EnemyDeath;
+// 	public GameObject EnemyDeath;
 
-	public GameObject ProjectileParticle;
+// 	public GameObject ProjectileParticle;
 
-	public int PointsForKill;
+// 	public int PointsForKill;
 
-	// Use this for initialization
-	void Start () {
+// 	// Use this for initialization
+// 	void Start () {
 		
-			if(PC.transform.localScale.x < 0)
-					Speed = -Speed;
+// 			if(PC.transform.localScale.x < 0)
+// 					Speed = -Speed;
 
-					// Speed = Speed * Mathf.Sign(PC.transform.localScale.x);
+// 					// Speed = Speed * Mathf.Sign(PC.transform.localScale.x);
 
-					// GetComponent<Rigidbody2D>().velocity = new Vector2(Speed + (PC.GetComponent<Rigidbody2D>().velocity.x/3), GetComponent<Rigidbody2D>().velocity.y + (PC.GetComponent<Rigidbody2D>().velocity.y/3))
+// 					// GetComponent<Rigidbody2D>().velocity = new Vector2(Speed + (PC.GetComponent<Rigidbody2D>().velocity.x/3), GetComponent<Rigidbody2D>().velocity.y + (PC.GetComponent<Rigidbody2D>().velocity.y/3))
 
 
-	}
+// 	}
 	
-	// Update is called once per frame
-	void Update () {
-			GetComponent<Rigidbody2D>().velocity = new Vector2(Speed, GetComponent<Rigidbody2D>().velocity.y);
-	}
+// 	// Update is called once per frame
+// 	void Update () {
+// 			GetComponent<Rigidbody2D>().velocity = new Vector2(Speed, GetComponent<Rigidbody2D>().velocity.y);
+// 	}
 
-	void OnTriggerEnter2D(Collider2D other){
-			if(other.tag == "Enemy"){
-					Instantiate(EnemyDeath, other.transform.position, other.transform.rotation);
-					Destroy (other.GameObject);
-					ScoreManager.AddPoints (PointsForKill);
-			}
+// 	void OnTriggerEnter2D(Collider2D other){
+// 			if(other.tag == "Enemy"){
+// 					Instantiate(EnemyDeath, other.transform.position, other.transform.rotation);
+// 					Destroy (other.GameObject);
+// 					ScoreManager.AddPoints (PointsForKill);
+// 			}
 
 
-			Instantiate(ProjectileParticle, transform.position, transform.rotation);
-			Destroy (GameObject);
-	}
-}
+// 			Instantiate(ProjectileParticle, transform.position, transform.rotation);
+// 			Destroy (GameObject);
+// 	}
+// }
