53a9990f30dca296a8d4a6058473d3b68ebf1c5a
Daniel Bredthauer <dcarlbred@gmail.com>
2018-11-05 17:11:03 -0700

    Added CameraFollow scritp

---
+++ My Game Fall 2018/Assets/Script/CameraFollow.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour {
+
+	public CharacterMove Player;
+
+	public bool IsFollowing;
+
+
+	// Camera position off set
+	public float xOffset;
+	public float yOffset;
+
+	// Use this for initialization
+	void Start () {
+		Player = FindObjectOfType<CharacterMove>();
+
+		IsFollowing = true;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(IsFollowing){
+			transform.position = new Vector3(Player.transform.position.x + xOffset, Player.transform.position.y + yOffset, transform.position.z);
+			
+		}
+	}
+}
--- My Game Fall 2018/Assets/Script/Practice Scrpts/Practice Switch.cs
+++ My Game Fall 2018/Assets/Script/Practice Scrpts/Practice Switch.cs
@@ -1,91 +1,91 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿// using System.Collections;
+// using System.Collections.Generic;
+// using UnityEngine;
 
-public class PracticeSwitch : MonoBehaviour {
+// public class PracticeSwitch : MonoBehaviour {
 
-	public int Apples;
-	public float Cloud;
-	public string Colors;
-	public int People;
-	public float Fireworks;
+// 	public int Apples;
+// 	public float Cloud;
+// 	public string Colors;
+// 	public int People;
+// 	public float Fireworks;
 
-	// Use this for initialization
-	void Start () {
+// 	// Use this for initialization
+// 	void Start () {
 
-		switch(Apples){
+// 		switch(Apples){
 
-			case 1:
-				print("There is " + Apples + " apple!");
-			break;
+// 			case 1:
+// 				print("There is " + Apples + " apple!");
+// 			break;
 
-			case 5:
-				print("There are " + Apples + " apples!");
-			break;
+// 			case 5:
+// 				print("There are " + Apples + " apples!");
+// 			break;
 
-			case 7:
-				print("There are " + Apples + " apples!");
-			break;
+// 			case 7:
+// 				print("There are " + Apples + " apples!");
+// 			break;
 
-			case 15:
-				print("There are " + Apples + " apples!");
-			break;
+// 			case 15:
+// 				print("There are " + Apples + " apples!");
+// 			break;
 
-			default:
-				print("I can't eat " + Apples + " apples!");
-			break;
+// 			default:
+// 				print("I can't eat " + Apples + " apples!");
+// 			break;
 
-		}
-		switch(Cloud){
+// 		}
+// 		switch(Cloud){
 
-			case 1:
-				print("The cloud is moving " + Cloud + " mph.");
-			break;
+// 			case 1:
+// 				print("The cloud is moving " + Cloud + " mph.");
+// 			break;
 
-			case 4:
-				print("The cloud is moving " + Cloud + " mph.");
-			break;
+// 			case 4:
+// 				print("The cloud is moving " + Cloud + " mph.");
+// 			break;
 
-			case 10:
-				print("The cloud is moving " + Cloud + " mph.");
-			break;
+// 			case 10:
+// 				print("The cloud is moving " + Cloud + " mph.");
+// 			break;
 
-			case 24:
-				print("The cloud is moving " + Cloud + " mph.");
-			break;
+// 			case 24:
+// 				print("The cloud is moving " + Cloud + " mph.");
+// 			break;
 
-			case 55:
-				print("The cloud is moving " + Cloud + " mph.");
-			break;
+// 			case 55:
+// 				print("The cloud is moving " + Cloud + " mph.");
+// 			break;
 
-			default:
-				print("Tut tut! Looks like rain!");
-			break;
+// 			default:
+// 				print("Tut tut! Looks like rain!");
+// 			break;
 
-		}
-		switch(Colors){
+// 		}
+// 		switch(Colors){
 
-			case Red:
-				print("It is " + Colors);
-			break;
+// 			case Red:
+// 				print("It is " + Colors);
+// 			break;
 
-			case Blue:
-				print("It is " + Colors);
-			break;
+// 			case Blue:
+// 				print("It is " + Colors);
+// 			break;
 
-			case Yellow:
-				print("It is " + Colors);
-			break;
+// 			case Yellow:
+// 				print("It is " + Colors);
+// 			break;
 
-			case Green:
-				print("It is " + Colors);
-			break;
+// 			case Green:
+// 				print("It is " + Colors);
+// 			break;
 
-			default:
-				print("I don't know " + Colors);
-			break;
+// 			default:
+// 				print("I don't know " + Colors);
+// 			break;
 
 
-	}
+// 	}
 
-}
+// }
